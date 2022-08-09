7698527f13e491170a095f2939ce57b7d8aaa10c
Danilo <dundeepessoal@gmail.com>
2022-08-09 11:17:55 -0300

    :recycle: Changing to tuirealm

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,51 @@
-use std::{io, thread, time::Duration};
+extern crate tuirealm;
 
-use tui::{
-    backend::{Backend,CrosstermBackend},
-    widgets::{Widget, Block, Borders},
-    layout::{Layout, Constraint, Direction},
-    Frame,
-    Terminal
-};
+use tuirealm::application::PollStrategy;
+use tuirealm::{AttrValue, Attribute, Update};
+use tuirealm::terminal::TerminalBridge;
+use tuirealm::tui::layout::{Constraint, Direction, Layout};
+use tuirealm::tui::widgets::{Block, Borders};
 
-use crossterm::{
-    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
-    execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-};
+use std::{thread, time::Duration};
 
-fn main() -> Result<(), io::Error> {
+fn main() {
+    let mut terminal = TerminalBridge::new().expect("Deu não meu cria");
+    terminal.enter_alternate_screen();
+    terminal.enable_raw_mode();
 
-    enable_raw_mode()?;
-    let mut stdout = io::stdout();
-    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
+    terminal
+        .raw_mut()
+        .draw(|f| {
+            let chunks = Layout::default()
+                .direction(Direction::Horizontal)
+                .margin(1)
+                .constraints(
+                    [
+                    Constraint::Percentage(60),
+                    Constraint::Percentage(40)
+                    ]
+                    .as_ref()
+                    )
+                .split(f.size());
 
-    terminal.draw(ui)?;
+            f.render_widget(
+                Block::default()
+                .title("SINGLE REQUEST VIEW")
+                .borders(Borders::ALL)
+                , chunks[0]);
 
-    thread::sleep(Duration::from_millis(5000));
+            f.render_widget(
+                Block::default()
+                .title("REQUEST LOG VIEW")
+                .borders(Borders::ALL)
+                , chunks[1]);
+        })
+    .is_ok();
 
-    disable_raw_mode()?;
-    execute!(
-        terminal.backend_mut(),
-        LeaveAlternateScreen,
-        DisableMouseCapture
-    )?;
-    terminal.show_cursor();
+    thread::sleep(Duration::from_millis(2000));
 
-    Ok(())
+    terminal.leave_alternate_screen();
+    terminal.disable_raw_mode();
+    terminal.clear_screen();
 }
 
-fn ui<B: Backend>(f: &mut Frame<B>) {
-    let chunks = Layout::default()
-        .direction(Direction::Horizontal)
-        .margin(1)
-        .constraints(
-            [
-                Constraint::Percentage(60),
-                Constraint::Percentage(40)
-            ].as_ref()
-        )
-        .split(f.size());
-
-    let single_request_menu = Block::default()
-        .title("REQUEST")
-        .borders(Borders::ALL);
-
-    f.render_widget(single_request_menu, chunks[0]);
-
-    let request_log_menu = Block::default()
-        .title("LOG")
-        .borders(Borders::ALL);
-    
-    f.render_widget(request_log_menu, chunks[1]);
-}
